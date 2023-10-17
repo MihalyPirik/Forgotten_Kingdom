498aa122333d5dfe9952ed150e6b45db2a3e41e1
PinterArminjedlik <pinter.armin@students.jedlik.eu>
2023-10-17 17:06:27 +0200

    Databse implementaion

---
+++ database_connection/Databse dump.sql
@@ -0,0 +1,255 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: forgottenkingdom
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `buildings`
+--
+
+DROP TABLE IF EXISTS `buildings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `buildings` (
+  `building_id` int NOT NULL,
+  `building_name` varchar(45) NOT NULL,
+  `level` int NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `city_id` int NOT NULL,
+  PRIMARY KEY (`building_id`),
+  KEY `building-city_idx` (`city_id`),
+  CONSTRAINT `building-city` FOREIGN KEY (`city_id`) REFERENCES `cities` (`city_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buildings`
+--
+
+LOCK TABLES `buildings` WRITE;
+/*!40000 ALTER TABLE `buildings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `buildings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cities`
+--
+
+DROP TABLE IF EXISTS `cities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cities` (
+  `city_id` int NOT NULL,
+  `city_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`city_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cities`
+--
+
+LOCK TABLES `cities` WRITE;
+/*!40000 ALTER TABLE `cities` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `item_id` int NOT NULL,
+  `itemLevel_id` int NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `item-itemlevel_idx` (`itemLevel_id`),
+  CONSTRAINT `item-itemlevel` FOREIGN KEY (`itemLevel_id`) REFERENCES `itemlevel` (`itemLevel_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `itemlevel`
+--
+
+DROP TABLE IF EXISTS `itemlevel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `itemlevel` (
+  `itemLevel_id` int NOT NULL,
+  `item_level` int NOT NULL,
+  `item_attack` int NOT NULL,
+  PRIMARY KEY (`itemLevel_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `itemlevel`
+--
+
+LOCK TABLES `itemlevel` WRITE;
+/*!40000 ALTER TABLE `itemlevel` DISABLE KEYS */;
+/*!40000 ALTER TABLE `itemlevel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `players` (
+  `player_id` int NOT NULL,
+  `player_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `city_id` int NOT NULL,
+  `item_id` int NOT NULL,
+  `HP` int NOT NULL DEFAULT '0',
+  `stone_count` int NOT NULL DEFAULT '0',
+  `wood_count` int NOT NULL DEFAULT '0',
+  `coal_count` int NOT NULL DEFAULT '0',
+  `iron_count` int NOT NULL DEFAULT '0',
+  `wheat_count` int NOT NULL DEFAULT '0',
+  `fish_count` int NOT NULL DEFAULT '0',
+  `money_count` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`player_id`),
+  KEY `city-name_idx` (`city_id`),
+  KEY `player-item_idx` (`item_id`),
+  CONSTRAINT `player-city` FOREIGN KEY (`city_id`) REFERENCES `cities` (`city_id`),
+  CONSTRAINT `player-item` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `players`
+--
+
+LOCK TABLES `players` WRITE;
+/*!40000 ALTER TABLE `players` DISABLE KEYS */;
+/*!40000 ALTER TABLE `players` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questimprovement`
+--
+
+DROP TABLE IF EXISTS `questimprovement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `questimprovement` (
+  `quest_id` int NOT NULL,
+  `player_id` int NOT NULL,
+  `IsCompleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`quest_id`),
+  KEY `improvement-player_idx` (`player_id`),
+  CONSTRAINT `improvement-player` FOREIGN KEY (`player_id`) REFERENCES `players` (`player_id`),
+  CONSTRAINT `improvement-quest` FOREIGN KEY (`quest_id`) REFERENCES `quests` (`quest_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questimprovement`
+--
+
+LOCK TABLES `questimprovement` WRITE;
+/*!40000 ALTER TABLE `questimprovement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `questimprovement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quests`
+--
+
+DROP TABLE IF EXISTS `quests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `quests` (
+  `quest_id` int NOT NULL,
+  `quest_name` varchar(45) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`quest_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quests`
+--
+
+LOCK TABLES `quests` WRITE;
+/*!40000 ALTER TABLE `quests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `quests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `residents`
+--
+
+DROP TABLE IF EXISTS `residents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `residents` (
+  `npc_id` int NOT NULL,
+  `npc_name` varchar(45) NOT NULL,
+  `buidling_id` int NOT NULL,
+  `job_name` varchar(45) NOT NULL,
+  `quest_id` int DEFAULT NULL,
+  PRIMARY KEY (`npc_id`),
+  KEY `resident-building_idx` (`buidling_id`),
+  KEY `resident-quest_idx` (`quest_id`),
+  CONSTRAINT `resident-building` FOREIGN KEY (`buidling_id`) REFERENCES `buildings` (`building_id`),
+  CONSTRAINT `resident-quest` FOREIGN KEY (`quest_id`) REFERENCES `quests` (`quest_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `residents`
+--
+
+LOCK TABLES `residents` WRITE;
+/*!40000 ALTER TABLE `residents` DISABLE KEYS */;
+/*!40000 ALTER TABLE `residents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'forgottenkingdom'
+--
+
+--
+-- Dumping routines for database 'forgottenkingdom'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-17 16:23:02
