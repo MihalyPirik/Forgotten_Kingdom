334e7c8206053f517161f72239ed88509048f997
Pinter Armin <pinter.armin@students.jedlik.eu>
2024-01-31 11:11:12 +0100

    localhost

--- Frontend/services/apiService.js
+++ Frontend/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-
-axios.defaults.baseURL='http://localhost:3000'
+// https://bgs.jedlik.eu/81000
+axios.defaults.baseURL='http://127.0.0.1:8100'
 
 const FormRegistration = async(name, email, password) => {
   return axios.post('/user/registration', ({ "name": name, "email": email, "password": password }))
