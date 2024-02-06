9ca7576f9f5cfd74baf293131d09bb8dd1ce4c3e
Pinter Armin <pinter.armin@students.jedlik.eu>
2024-02-06 13:13:47 +0100

    Backend player endpoints

--- Backend/Controllers/gameController/playerController.js
+++ Backend/Controllers/gameController/playerController.js
@@ -1,9 +1,58 @@
 const Player = require("../../models/player")
-
+const Enemy= require('../../models/enemy')
+const Resident= require('../../models/resident')
+const QuestStatistics= require('../../models/questStatistics')
+const EnemyType= require('../../models/enemyType')
+const Quest=require('../../models/quest')
 const getAllData=async(req,res,next)=>
 {
-const data=await Player.findOne({where:{player_id:req.params.playerId},attributes:{exclude:['player_id','password','email']}})
-res.status(200).json({"data":data})
+    try {
+        const data=await Player.findOne({where:{player_id:req.params.playerId},attributes:{exclude:['player_id','password','email']}})
+        res.status(200).json({"data":data}) 
+    } catch (error) {
+        next(error)
+    }
+
 }
 
-module.exports={getAllData}
+// tools, enemies, residents, quest statistics
+const getEnemies=async(req,res,next)=>
+{
+    try {
+        const data=await Enemy.findAll({where:{world_id:req.params.playerId},attributes:{exclude:['world_id']},include:{model:EnemyType}})
+        res.status(200).json({"data":data})
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+}
+
+const getResidents=async(req,res,next)=>
+{
+    try {
+        const data=await Resident.findAll({where:{world_id:req.params.playerId},attributes:{exclude:['world_id'],include:Quest}})
+        res.status(200).json({"data":data})
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getQuests=async(req,res,next)=>
+{
+    try {
+        const data=await QuestStatistics.findAll({where:{player_id:req.params.playerId},include:Quest})
+        res.status(200).json({"data":req.params.playerId})
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getInventory=async(req,res,next)=>
+{
+    try {
+        const data=await Player.findByPk(req.params.playerId)
+    } catch (error) {
+        
+    }
+}
+module.exports={getAllData,getEnemies,getResidents,getQuests,getInventory}
--- Backend/routes/gameRoutes/playerRoutes.js
+++ Backend/routes/gameRoutes/playerRoutes.js
@@ -1,4 +1,4 @@
-const { getAllData } = require('../../controllers/gameController/playerController')
+const { getAllData, getEnemies, getResidents, getQuests, getInventory } = require('../../controllers/gameController/playerController')
 const { userAuth } = require('../../middlewares/auth')
 
 const playerRouter=require('express').Router({mergeParams:true})
@@ -6,5 +6,8 @@ const playerRouter=require('express').Router({mergeParams:true})
 // playerRouter.use(userAuth)
 
 playerRouter.get('/',getAllData)
-
+playerRouter.get('/enemies',getEnemies)
+playerRouter.get('/residents',getResidents)
+playerRouter.get('/quests',getQuests)
+playerRouter.get('/inventory',getInventory)
 module.exports=playerRouter
