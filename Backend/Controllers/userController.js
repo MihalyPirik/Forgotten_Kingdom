6781b910001578c118d52c92b68fbafcb15a6004
Mihaly Pirik <misipirik@gmail.com>
2024-02-07 12:13:42 +0100

    backend login error handling

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const Player = require('../models/player')
-const uuid=require('uuid')
+const uuid = require('uuid')
 const { ValidationError, UniqueConstraintError } = require('sequelize')
 
 
@@ -11,53 +11,52 @@ const registration = async (req, res, next) => {
         const email = req.body.email
         const password = req.body.password
         const name = req.body.name
-        await Player.create({player_id:uuid.v1(),email:email,password:password,player_name:name})
+        await Player.create({ player_id: uuid.v1(), email: email, password: password, player_name: name })
 
         res.status(201).json({ "message": "Sikeres regisztráció!" })
     } catch (error) {
         console.log(error);
         if (error instanceof ValidationError) {
-            res.status(400).json({"message": error.errors[0].message})
+            res.status(400).json({ "message": error.errors[0].message })
             console.log(error.errors[0].message);
         }
         else if (error instanceof UniqueConstraintError) {
-            res.status(400).json({"message": error.message})
+            res.status(400).json({ "message": error.message })
         }
-        else
-        {
+        else {
             next(error)
         }
     }
 }
 
-const login=async(req,res,next)=>
-{
-try {
-    const {email,password}=req.body
-const user=await Player.findOne({where:{email:email}})
-if(user)
-{
-        if(user.comparePassword(password))
-        {
-            console.log(user);
-            const token=jwt.sign({id:user.player_id},process.env.SECRET_KEY,{expiresIn:"1h"})
-            res.setHeader("Authorization",`Bearer ${token}`)
-            res.status(200).json({"message":"Sikeres bejelentkezés!"})
-        }
-        else
-        {
-            res.status(401).json({"message":"Hibás jelszó!"})
-        }
+const login = async (req, res, next) => {
+    const { email, password } = req.body
+    if (email == undefined) {
+        return res.status(401).json({ "message": "Kérjük adja meg az email címét." })
     }
-    else
-    {
-        res.status(401).json({"message":"Hibás email cím!"})
+    if (password == undefined) {
+        return res.status(401).json({ "message": "Kérjük adja meg a jelszavát." })
     }
-} catch (error) {
-    console.log(error);
-    next(error)
-}
-    
+    try {
+        const user = await Player.findOne({ where: { email: email } })
+        if (user) {
+            if (await user.comparePassword(password)) {
+                const token = jwt.sign({ id: user.player_id }, process.env.SECRET_KEY, { expiresIn: "1h" })
+                res.setHeader("Authorization", `Bearer ${token}`)
+                res.status(200).json({ "message": "Sikeres bejelentkezés!" })
+            }
+            else {
+                res.status(401).json({ "message": "Hibás jelszó!" })
+            }
+        }
+        else {
+            res.status(401).json({ "message": "Hibás email cím!" })
+        }
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+
 }
 
-module.exports = { registration,login }
+module.exports = { registration, login }
--- Backend/models/player.js
+++ Backend/models/player.js
@@ -1,178 +1,189 @@
 const { DataTypes } = require("sequelize")
-const bcrypt=require('bcrypt')
+const bcrypt = require('bcrypt')
 const dbConnection = require("../services/dbService")
 
 
 
 
-    const Player=dbConnection.define
+const Player = dbConnection.define
     (
         'Player',
         {
             player_id:
             {
-                primaryKey:true,
-                type:DataTypes.UUID
+                primaryKey: true,
+                type: DataTypes.UUID
             },
             player_name:
             {
-                type:DataTypes.STRING,
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: "Kötelező a név megadása!"
+                    },
+                    notEmpty: {
+                        msg: "Kötelező a név megadása!"
+                    },
+                    len: {
+                        args: [4],
+                        msg: "A névnek minimum 4 karakter hosszúnak kell lennie!"
+                    }
+
+                }
             },
             email:
             {
-                type:DataTypes.STRING,
-                allowNull:false,
-                unique:{msg: "Ez az email cím már foglalt"},
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: { msg: "Ez az email cím már foglalt" },
                 validate:
                 {
                     isEmail:
                     {
-                        msg:"Nem megfelelő email cím!"
+                        msg: "Nem megfelelő email cím!"
                     },
                     notNull:
                     {
-                        msg:"Az email cím megadása kötelező!"
+                        msg: "Az email cím megadása kötelező!"
                     },
                     notEmpty:
                     {
-                        msg:"Az email cím megadása kötelező!"
+                        msg: "Az email cím megadása kötelező!"
                     }
                 }
             },
             password:
             {
-                type:DataTypes.STRING,
-                allowNull:false,
+                type: DataTypes.STRING,
+                allowNull: false,
                 validate:
                 {
                     notNull:
                     {
-                        msg:"A jelszó megadása kötelező!"
+                        msg: "A jelszó megadása kötelező!"
                     },
                     notEmpty:
                     {
-                        msg:"A jelszó megadása kötelező!"
+                        msg: "A jelszó megadása kötelező!"
                     },
                     len:
                     {
-                        args:[8,20],
-                        msg:"A jelszónak 8 és 20 karakter között kell lennie!"
+                        args: [8, 20],
+                        msg: "A jelszónak 8 és 20 karakter között kell lennie!"
                     }
                 }
             },
             HP:
             {
-                type:DataTypes.DOUBLE,
-                allowNull:false,
-                defaultValue:100,
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+                defaultValue: 100,
                 validate:
                 {
-                    min:0,
-                    max:100
+                    min: 0,
+                    max: 100
                 }
             },
             money:
             {
-                type:DataTypes.DOUBLE,
-                allowNull:false,
-                defaultValue:100,
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+                defaultValue: 100,
                 validate:
                 {
-                    min:0
+                    min: 0
                 }
             },
             world_name:
             {
-                type:DataTypes.STRING,
-                defaultValue:'New World'
+                type: DataTypes.STRING,
+                defaultValue: 'New World'
             },
             stone:
             {
-                type:DataTypes.INTEGER,
-                allowNull:false,
-                defaultValue:100
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 100
             },
             wood:
             {
-                type:DataTypes.INTEGER,
-                allowNull:false,
-                defaultValue:100
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 100
             },
             coal:
             {
-                type:DataTypes.INTEGER,
-                allowNull:false,
-                defaultValue:100
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 100
             },
             iron:
             {
-                type:DataTypes.INTEGER,
-                allowNull:false,
-                defaultValue:100
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 100
             },
             wheat:
             {
-                type:DataTypes.INTEGER,
-                allowNull:false,
-                defaultValue:100
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 100
             },
             fish:
             {
-                type:DataTypes.INTEGER,
-                allowNull:false,
-                defaultValue:100
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 100
             },
             objX:
             {
-                type:DataTypes.DOUBLE,
-                allowNull:false,
-                defaultValue:0
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+                defaultValue: 0
             },
             objY:
             {
-                type:DataTypes.DOUBLE,
-                allowNull:false,
-                defaultValue:0
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+                defaultValue: 0
             },
             blockX:
             {
-                type:DataTypes.INTEGER,
-                allowNull:false,
-                defaultValue:0
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 0
             },
             blockY:
             {
-                type:DataTypes.INTEGER,
-                allowNull:false,
-                defaultValue:0
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 0
             }
         },
         {
-            tableName:'players',
-            timestamps:false,
+            tableName: 'players',
+            timestamps: false,
             hooks:
             {
-                beforeCreate:async(user)=>
-                {
-                    user.password=await bcrypt.hash(user.password,await bcrypt.genSalt(10))
+                beforeCreate: async (user) => {
+                    user.password = await bcrypt.hash(user.password, await bcrypt.genSalt(10))
                 }
             },
         }
     )
-    Player.associate=(models)=>
-    {
-Player.belongsToMany(models.ToolType,{through:models.Tool,foreignKey:"player_id"})
-Player.belongsToMany(models.Quest,{through:models.QuestStatistics,foreignKey:"player_id"})
-Player.belongsToMany(models.EnemyType,{through:models.Enemy,foreignKey:"world_id"})
-Player.hasMany(models.Resident,{foreignKey:"world_id"})
-Player.hasMany(models.Tool,{foreignKey:'player_id',as:'tool'})
-Player.hasMany(models.Enemy,{foreignKey:'world_id',as:'world'})
-Player.hasMany(models.QuestStatistics,{foreignKey:'player_id',as:'playerQuest'})
-    }
-
-Player.prototype.comparePassword=async function(password)
-{
-    return await bcrypt.compare(password,this.password)
+Player.associate = (models) => {
+    Player.belongsToMany(models.ToolType, { through: models.Tool, foreignKey: "player_id" })
+    Player.belongsToMany(models.Quest, { through: models.QuestStatistics, foreignKey: "player_id" })
+    Player.belongsToMany(models.EnemyType, { through: models.Enemy, foreignKey: "world_id" })
+    Player.hasMany(models.Resident, { foreignKey: "world_id" })
+    Player.hasMany(models.Tool, { foreignKey: 'player_id', as: 'tool' })
+    Player.hasMany(models.Enemy, { foreignKey: 'world_id', as: 'world' })
+    Player.hasMany(models.QuestStatistics, { foreignKey: 'player_id', as: 'playerQuest' })
 }
 
-module.exports=Player
+Player.prototype.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+module.exports = Player
