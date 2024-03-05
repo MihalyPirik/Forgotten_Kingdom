0c0bb9ed42a4b09360f5e0f23739b55b9592b621
Pinter Armin <pinter.armin@students.jedlik.eu>
2024-03-05 13:23:43 +0100

    add more blocks

--- Frontend/GamePage/isometricBlocks/farm.js
+++ Frontend/GamePage/isometricBlocks/farm.js
@@ -1,1 +1,55 @@
-export const Farm=()=>{console.log('Farm');}
+import { Circle } from "../models/Circle.js"
+import { Line } from "../models/Line.js"
+import { Panel } from "../models/Panel.js"
+import { Point } from "../models/Point.js"
+import { IsometricBlock } from "../models/isometricBlock.js"
+
+
+
+const backGround=new Image()
+backGround.src="./assets/blocks/Farm.png"
+
+export const Farm=(game)=>{
+
+    game.currentBlock=new IsometricBlock(
+        'Farm',
+        backGround,
+        null,
+        [
+
+        ],
+        [
+            // start: x: 0.951, y:0.628
+            // end: x: 0.714, y:0.749
+new Line(
+    new Point(game.width*0.507,game.height*0.89),
+new Point(game.width*0.673,game.height*0.798)),
+new Line(new Point(game.width*0.673,game.height*0.798),new Point(game.width*0.334,game.height*0.608)),
+new Line(new Point(game.width*0.507,game.height*0.89),new Point(game.width*0.025,game.height*0.623)),
+new Line(new Point(game.width*0.025,game.height*0.623),new Point(game.width*0.19,game.height*0.51)),
+new Line(new Point(game.width*0.19,game.height*0.51),new Point(game.width*0.289,game.height*0.568)),
+new Line(new Point(game.width*0.337,game.height*0.523),new Point(game.width*0.286,game.height*0.487)),
+new Line(new Point(game.width*0.286,game.height*0.487),new Point(game.width*0.326,game.height*0.446)),
+new Line(new Point(game.width*0.326,game.height*0.446),new Point(game.width*0.502,game.height*0.561)),
+new Line(new Point(game.width*0.502,game.height*0.561),new Point(game.width*0.548,game.height*0.536)),
+new Line(new Point(game.width*0.548,game.height*0.536),new Point(game.width*0.554,game.height*0.577)),
+new Line(new Point(game.width*0.554,game.height*0.577),new Point(game.width*0.634,game.height*0.617)),
+new Line(new Point(game.width*0.422,game.height*0.592),new Point(game.width*0.526,game.height*0.646)),
+new Line(new Point(game.width*0.567,game.height*0.673),new Point(game.width*0.714,game.height*0.749)),
+new Line(new Point(game.width*0.714,game.height*0.749),new Point(game.width*0.951,game.height*0.628)),
+new Line(new Point(game.width*0.567,game.height*0.673),new Point(game.width*0.634,game.height*0.617)),
+new Line(new Point(game.width*0.337,game.height*0.523),new Point(game.width*0.289,game.height*0.568)),
+new Line(new Point(game.width*0.673,game.height*0.798),new Point(game.width*0.714,game.height*0.749)),
+new Circle(game.width*0.567,game.height*0.673,game.width*0.01),
+new Circle(game.width*0.548,game.height*0.536,game.width*0.01),
+new Circle(game.width*0.502,game.height*0.561,game.width*0.01),
+new Circle(game.width*0.554,game.height*0.577,game.width*0.01),
+new Circle(game.width*0.289,game.height*0.568,game.width*0.01),
+new Circle(game.width*0.337,game.height*0.523,game.width*0.01)
+        ],
+        [
+            new Panel('navigationX','navigationPanel',game.width*0.7638,game.height*0.7574,game.width*0.2,{forwardId:'forward',backwardId:'backward',direction:'x'}),
+            new Panel('navigationY','navigationPanel',game.width*0.2425,game.height*0.7713,game.width*0.2,{forwardId:'forward',backwardId:'backward',direction:'y'})
+        ]
+    )
+}
--- Frontend/GamePage/isometricBlocks/fishingPond.js
+++ Frontend/GamePage/isometricBlocks/fishingPond.js
@@ -1,1 +1,53 @@
-export const Horgásztó=()=>{console.log('Horgásztó');}
+import { Line } from "../models/Line.js"
+import { Point } from "../models/Point.js"
+import { IsometricBlock } from "../models/isometricBlock.js"
+import { Circle } from '../models/Circle.js'
+
+const backGround=new Image()
+backGround.src="./assets/blocks/FishingPond.png"
+
+// start X: 0.841,  Y: 0.72
+// end X: , Y: 
+export const Horgásztó=(game)=>{
+    game.currentBlock=new IsometricBlock(
+        'Horgásztó',
+        backGround,
+        null,
+        [
+            
+        ],
+        [
+            new Line(new Point(game.width*0.496,game.height*0.909),new Point(game.width*0.016,game.height*0.651)),
+            new Circle(game.width*0.044,game.height*0.62,game.width*0.04),
+            new Circle(game.width*0.08,game.height*0.57,game.width*0.04),
+            new Circle(game.width*0.08,game.height*0.5,game.width*0.04),
+            new Circle(game.width*0.13,game.height*0.44,game.width*0.04),
+            new Line(new Point(game.width*0.155,game.height*0.434),new Point(game.width*0.273,game.height*0.415)),
+            new Line(new Point(game.width*0.273,game.height*0.415),new Point(game.width*0.348,game.height*0.453)),
+            new Line(new Point(game.width*0.348,game.height*0.453),new Point(game.width*0.476,game.height*0.4)),
+            new Line(new Point(game.width*0.476,game.height*0.4),new Point(game.width*0.521,game.height*0.305)),
+            new Circle(game.width*0.555,game.height*0.28,game.width*0.05),
+            new Line(new Point(game.width*0.577,game.height*0.31),new Point(game.width*0.625,game.height*0.388)),
+            new Circle(game.width*0.65,game.height*0.37,game.width*0.04),
+            new Circle(game.width*0.73,game.height*0.36,game.width*0.04),
+            new Circle(game.width*0.784,game.height*0.407,game.width*0.043),
+            new Circle(game.width*0.847,game.height*0.463,game.width*0.043),
+            new Circle(game.width*0.9,game.height*0.52,game.width*0.043),
+            new Circle(game.width*0.894,game.height*0.59,game.width*0.043),
+            new Circle(game.width*0.873,game.height*0.66,game.width*0.043),
+            new Circle(game.width*0.841,game.height*0.71,game.width*0.02),
+            new Line(new Point(game.width*0.496,game.height*0.909),new Point(game.width*0.841,game.height*0.72)),
+            new Circle(game.width*0.361,game.height*0.53,game.width*0.02),
+            new Circle(game.width*0.33,game.height*0.534,game.width*0.02),
+            new Circle(game.width*0.29,game.height*0.548,game.width*0.02),
+            new Circle(game.width*0.25,game.height*0.56,game.width*0.02),
+            new Circle(game.width*0.22,game.height*0.58,game.width*0.02),
+            new Circle(game.width*0.22,game.height*0.613,game.width*0.02),
+            new Circle(game.width*0.23,game.height*0.644,game.width*0.02)
+        ],
+        [
+
+        ]
+
+    )
+}
