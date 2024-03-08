f88c01b459db0f4dae4e15680a2d98b22738ee75
Mihaly Pirik <misipirik@gmail.com>
2024-03-08 08:48:37 +0100

    fixing put endpoints

--- Backend/Controllers/gameController/playerController.js
+++ Backend/Controllers/gameController/playerController.js
@@ -1,4 +1,6 @@
 const Player = require("../../models/player");
+const Tool = require("../../models/tool");
+const ToolType = require("../../models/toolType");
 
 const getAllData = async (req, res, next) => {
   try {
@@ -7,6 +9,16 @@ const getAllData = async (req, res, next) => {
     const data = await Player.findOne({
       where: { player_id: player_id },
       attributes: { exclude: ["player_id", "password", "email"] },
+      include: [
+        {
+          model: Tool,
+          attributes: { exclude: ["player_id", "tool_type_id"] },
+          include: {
+            model: ToolType,
+            attributes: { exclude: ["tool_type_id"] },
+          },
+        },
+      ],
     });
     res.status(200).json({ data: data });
   } catch (error) {
@@ -70,7 +82,7 @@ const putPlayer = async (req, res, next) => {
       }
     );
 
-    res.status(200).json({ data: {message: "Sikeres módosítás!"} });
+    res.status(200).json({ data: { message: "Sikeres módosítás!" } });
   } catch (error) {
     next(error);
   }
@@ -82,9 +94,9 @@ const deletePlayer = async (req, res, next) => {
 
     const isDeleted = await Player.destroy({ where: { player_id: player_id } });
     if (isDeleted == 0) {
-      return res.status(404).json({message: "Ilyen játékos nem létezik!"})
+      return res.status(404).json({ message: "Ilyen játékos nem létezik!" });
     }
-    res.status(200).json({ data: {message: "Sikeres törlés!"} });
+    res.status(200).json({ data: { message: "Sikeres törlés!" } });
   } catch (error) {
     next(error);
   }
