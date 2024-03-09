fad18b2c2a36c85a0e65f823badabdad48d670dd
PinterArminjedlik <pinter.armin@students.jedlik.eu>
2024-03-09 11:53:41 +0100

    uodate swagger

--- Backend/swagger.js
+++ Backend/swagger.js
@@ -1,11 +1,21 @@
-const swaggerAutogen = require('swagger-autogen')()
+const swaggerAutogen = require('swagger-autogen')({openapi: '3.0.0'})
 const outputFile = './swagger_output.json'
 const endpointsFiles = ["./server.js"]
 
 const options={
     info:{
-        title:"Forgotten Kingdom"
-    }
+        title:"Forgotten Kingdom",
+        description:"A dokumentációt a <b>swagger-autogen</b> generálta"
+    },
+    securityDefinitions: {
+        jwt: {
+          type: 'http',
+          in: 'header',
+          scheme:"bearer",
+          name: 'Authorization',
+          description: 'A felhasználó hitelesístésre használt tokenje'
+        }
+      }
 }
 
 swaggerAutogen(outputFile, endpointsFiles,options)
