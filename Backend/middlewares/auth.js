40947e837d5a21f98903c2b6de34b1f23e37c40b
PinterArminjedlik <pinter.armin@students.jedlik.eu>
2024-03-18 23:20:01 +0100

    little fixing

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -5,11 +5,11 @@ const userAuth = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
-      res.status(401).json({ message: "Jelentkezz be!" });
+      return res.status(401).json({ message: "Jelentkezz be!" });
     }
     jwt.verify(token, process.env.SECRET_KEY, {}, (err, decodedToken) => {
       if (err) {
-        res.status(403).json({ message: "Nincs jogod a következőre!" });
+        return res.status(403).json({ message: "Nincs jogod a következőre!" });
       } else {
         req.token = decodedToken;
         next();
