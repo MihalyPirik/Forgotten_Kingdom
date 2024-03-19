44e3566de770909eaa7e215d65ce996bfc5e7d4c
Pinter Armin <pinter.armin@students.jedlik.eu>
2024-03-19 11:12:13 +0100

    add global service

---
+++ Frontend/services/buildingService.js
@@ -0,0 +1,26 @@
+import { base } from "./base.js";
+
+
+// GET
+
+export const getAllBuilding = () => {
+  return base("/buildings",'GET')
+};
+
+// POST
+
+export const postBuilding = (data) => {
+  return base("/buildings", 'POST', data)
+};
+
+// PUT
+
+export const putBuilding = (buildingId, data) => {
+  return base("/buildings/" + buildingId, 'PUT' ,data)
+};
+
+// DELETE
+
+export const deleteBuilding = (buildingId) => {
+  return base("/buildings/" + buildingId, 'DELETE')
+};
